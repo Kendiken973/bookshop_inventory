a821dec0bbbfba773eb61a0607f6d7c32056af1a
Kendiken973 <kendisheila374@gmail.com>
2025-06-24 15:31:43 +0300

    Remove /create_db route after table setup

--- app.py
+++ app.py
@@ -346,10 +346,10 @@ def add_header(response):
 #with app.app_context():
     #db.create_all()
 
-@app.route('/create_db')
-def create_db():
-    db.create_all()
-    return "✅ PostgreSQL tables created!"
+#@app.route('/create_db')
+#def create_db():
+    #db.create_all()
+    #return "✅ PostgreSQL tables created!"
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
